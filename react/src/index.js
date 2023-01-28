8b59bd3cd0e534471ac6a04c3f20f0e0ec7e1440
Parkhi <parkhigarg277@gmail.com>
2023-01-28 17:57:54 +0530

    jsx

--- react/src/index.js
+++ react/src/index.js
@@ -10,7 +10,24 @@ import ReactDOM from "react-dom";
 //   document.getElementById("root")
 // );
 
+// ReactDOM.render(
+//   [<h1>Learning React</h1>, <p>Will do</p> , <p>New Day</p>],
+//   document.getElementById("root")
+// );
+
+// jsx
+const fname = "Parkhi";
+const lname = "Garg";
+
 ReactDOM.render(
-  [<h1>Learning React</h1>, <p>Will do</p> , <p>New Day</p>],
+  <>
+    {/* <h1>My name is {fname + " " + lname}</h1> */}
+    <h1>{`My name is ${fname}`}</h1>
+    <p>my luck number is {5 + 5}</p>
+  </>,
   document.getElementById("root")
 );
+
+// template literals
+
+console.log(`My name is ${fname}`);
