f1c1c84f9bdbc8debe19c56202de241a8cc9e567
Parkhi <parkhigarg277@gmail.com>
2023-02-09 23:22:45 +0530

    login form

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,56 +1,79 @@
 import React from "react";
-
-
-const SlotM = () => {
-    let x = "A";
-    let y = "B";
-    let z = "C";
-
-    if ((x === y) && (y === z)) {
-        return (
-            <>
-                <div className="slot_inner">
-                    <h1>
-                        {x} {y}{z}
-                    </h1>
-                    <h1>This is matching</h1>
-                    <hr />
-                </div>
-            </>
-        );
-
-        
-    }
-    else {
-        return (
-            <>
-                <div className="slot_inner">
-                    <h1>
-                        {x} {y}{z}
-                    </h1>
-                    <h1>This is matching</h1>
-                    <hr />
-                </div>
-            </>
-        );
-    }
-}
-
+import { useState } from "react";
 
 const App = () => {
+  // using useState
+  const [name, setName] = useState("");
+  const [fullName, setFullName] = useState("");
+  const inputEvent = (event) => {
+    console.log(event.target.value);
+    setName(event.target.value);
+  };
+  const submitButton = () => {
+    setFullName(name);
+  };
   return (
     <>
-      <h1>
-        {" "}
-        Welcome to <span style={{ fontWeight: "bold" }}>Slot machine Game</span>
-      </h1>
-          <div className="slotmachine">
-              <slotM x="A" y="B" z="C" />
-              <hr />
-              
+      <div>
+        <h1>Hello {fullName}</h1>
+        <input>
+          type = "text" placeHolder = "What is your name" onChange =
+          {inputEvent}
+        </input>
+
+        <button onClick={submitButton}> Click Me </button>
       </div>
     </>
   );
-}
+};
+// const SlotM = () => {
+//     let x = "A";
+//     let y = "B";
+//     let z = "C";
 
-    export default App;
+//     if ((x === y) && (y === z)) {
+//         return (
+//             <>
+//                 <div className="slot_inner">
+//                     <h1>
+//                         {x} {y}{z}
+//                     </h1>
+//                     <h1>This is matching</h1>
+//                     <hr />
+//                 </div>
+//             </>
+//         );
+
+//     }
+//     else {
+//         return (
+//             <>
+//                 <div className="slot_inner">
+//                     <h1>
+//                         {x} {y}{z}
+//                     </h1>
+//                     <h1>This is matching</h1>
+//                     <hr />
+//                 </div>
+//             </>
+//         );
+//     }
+// }
+
+// const App = () => {
+//   return (
+//     <>
+//       <h1>
+//         {" "}
+//         Welcome to <span style={{ fontWeight: "bold" }}>Slot machine Game</span>
+//       </h1>
+//           <div className="slotmachine">
+//               <slotM x="A" y="B" z="C" />
+//               <hr />
+
+//       </div>
+//     </>
+//   );
+// }
+
+export default App;
